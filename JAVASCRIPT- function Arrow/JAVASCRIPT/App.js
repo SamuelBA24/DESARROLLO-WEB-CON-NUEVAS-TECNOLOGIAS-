9c29e0b0fdcdabfe9c159821b6512cbd17aef32b
Samuel David Blanco Arango <142445500+SamuelBA24@users.noreply.github.com>
2024-02-28 17:05:48 -0500

    TareaFunctionApp.js

--- JAVASCRIPT- function Arrow/JAVASCRIPT/App.js
+++ JAVASCRIPT- function Arrow/JAVASCRIPT/App.js
@@ -1,60 +1,31 @@
-//Declara variable sin actualizar
-let nombreUsuario;
-// declaración inicial
-let user = "Juan"
+// Sin Parametros
+const Sinparametro = () => {
+    console.log("Hola, me presento. Soy una función sin parámetros. No recibo nada.");
+    };
 
-console.log(nombreUsuario);
-console.log(user);
+    // Con Parametro
+    const funcionConUnParametro = (Parametro) => {
+    console.log("Hola, me presento. Soy una función con un parámetro. Recibo un parámetro: " + Parametro);
+    };
 
-// Operador ternario
+    //Con tres Parametros
+    const funcionConTresParametros = (parametro1, parametro2, parametro3) => {
+        let suma = parametro1 + parametro2 + parametro3;
+        console.log(`¡Hola, soy una función con tres parámetros! La suma de los parámetros es: ${suma}`);
+    };
 
-user==="Pedro"? console.log("Verdadero") : console.log("Falso"); 
+    //Declaración de variables con let y const
+    let edad = 19;
+    const ciudad =  "Madrid";
+    let nombre = "Samuel";
+    let saldo = 230000;
+    const producto = "Jacket";
+    let disponible = true;
 
-//Const Es parecido a constante
-
-const PI=3.1416
-//Trato de cambiar el valor
-//PI = 20;
-
-//Metodos propios del lenguaje
-let PI_DOS;
-PI_DOS = Math.PI;
-user.includes("P");
-console.log(user.length);
-//Mertodos o funciones
-//Arrow function
-
-const saludarUsuario = ()  => {
-    return "Hola Juanito";
-}
-
-const saludarUser = nombreUsuario =>{
-    return "Hola" +nombreUsuario;
-}
-
-const sumarDatos =(NumberOne, NumberTwo)=>{
-    return NumberOne+NumberTwo;
-}
-
-console.log(sumarDatos(1.0,2));
-
-// instancia de  una  función. Hacwe  lo mismo
-
-let tres = saludarUser;
-console.log(tres("Pedro"));
-
-//ciclos
-
-for (i=0;  i<10;i++){
-    
-}
-
-// Funcion de edad arrow function
-//
-
-const edadUsuario = () => {
-    return new Date().getFullYear() - 1995;
-}
-
-console.log(edadUsuario());
-
+    // Llamado a las funciones
+    console.log(edad);
+    console.log(ciudad);
+    console.log(nombre);
+    console.log(saldo);
+    console.log(producto);
+    console.log(disponible);
